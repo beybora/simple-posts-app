4f9b5b38b4fb0980b8e531ba683bcc4844b47d3a
beybora <bora.b.sarikaya@gmail.com>
2025-05-06 20:52:03 +0200

    load posts on homepage via postcontroller index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return inertia('Home');
 });
+
+Route::get('/', [PostController::class, 'index']);
+
+Route::resource('posts', PostController::class)->except('inex');
