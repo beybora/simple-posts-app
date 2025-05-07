26e8a5531d2dd71111d8a1f63b368b1bf19b2131
beybora <bora.b.sarikaya@gmail.com>
2025-05-07 18:02:58 +0200

    create post detail page

---
+++ resources/js/Pages/Show.jsx
@@ -0,0 +1,33 @@
+import { useForm } from "@inertiajs/react";
+import { useRoute } from "../../../vendor/tightenco/ziggy";
+
+export default function Show({ post }) {
+    const { delete: destroy } = useForm();
+    const route = useRoute();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        destroy(route("posts.destroy", post));
+    };
+
+    return (
+        <>
+            <div className="p-4 border-b">
+                <div className="text-sm text-gray-500">
+                    <span>
+                        <span>Postet on: </span>
+                        {new Date(post.created_at).toLocaleTimeString()}
+                    </span>
+                </div>
+                <p className="font-medium">{post.body}</p>
+                <div className="flex items-center justify-end gap-2">
+                    <form onSubmit={handleSubmit}>
+                        <button className="bg-red-500 rounded-md text-sm px-4 py-1 text-white cursor-pointer">
+                            Delete
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
